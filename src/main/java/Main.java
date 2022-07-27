1682c2b11f7887310036284ec373d0409fc5e58d
Angel Concepcion <angelconcepcion50@gmail.com>
2022-07-27 12:16:56 -0400

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,17 @@ import java.util.concurrent.Callable;
 public class Main {
     public static Callable<Integer> getCallableInteger() {
         // your code here
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+//        return new Callable<Integer>() {
+//            @Override
+//            public Integer call() throws Exception {
+//                return num;
+//            }
+//        };
+        System.out.println("hi");
+        return null;
     }
+    getCallableInteger();
 }
